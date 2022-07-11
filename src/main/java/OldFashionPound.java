704aa8a094d542f9cb5b16689abdc001c8c74a68
Marcello <marcello.cescutti@hotmail.it>
2022-07-11 12:37:33 +0200

    Creata regex nel Main

--- src/main/java/OldFashionPound.java
+++ src/main/java/OldFashionPound.java
@@ -3,6 +3,12 @@ public class OldFashionPound {
 
   public static void main(String[] args) {
 
+    String leftOperator = "\\s*\\d+p\\s*\\d+s\\s*\\d+d";
+    String operation = "\\+-*";
+    String rightOperator = "\\s*\\d+p\\s*\\d+s\\s*\\d+d";
+
+
+
 
 
 
--- src/main/java/service/Operations.java
+++ src/main/java/service/Operations.java
@@ -17,6 +17,7 @@ public class Operations implements OperationInterface {
 
     }
 
+
     @Override
     public void sum() {
 
